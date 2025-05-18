efccc136b6b5db4c42f13110bec3536954dbb1c4
mvalver2 <mvalver2@jhu.edu>
2025-05-18 02:44:42 -0400

    Got some of the basic home page things done still need over half of it

---
+++ src/components/sections/Home.jsx
@@ -0,0 +1,37 @@
+
+export const Home = () => {
+
+    return <section id="home" className="min-h-screen flex items-center justify-center relative">
+        <div className="text-center z-10 px-4">
+            <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent leading-right">
+                Hi, I'm Maria Valverde-Cervantes
+            </h1>
+
+        <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
+            I’m a senior at Johns Hopkins University majoring in Computer Science and minoring in Marketing and Communications. 
+            I'm passionate about web and app development, and I enjoy building creative, user-focused digital experiences. 
+            Outside of code, I enjoy reading, 3D printing, and building legos.     
+        </p>
+        <div className="flex justify-center space-x-4">
+            <a
+                href="#projects"
+                className="transform bg-blue-500 text-white py-3 px-6 rounded font-medium transition 
+                hover:-translate-y-0.5 
+                hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
+            >
+                View Projects
+            </a>
+
+            <a
+                href="#contact"
+                className="transform border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium 
+                transition-all duration-200 hover:-translate-y-0.5 
+                hover:shadow-[0_0_15px_rgba(59,130,246,0.2)] hover:bg-blue-500/10"
+            >
+                Contact Me
+            </a>
+        </div>
+
+        </div>
+    </section>
+};
